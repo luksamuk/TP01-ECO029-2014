c6a1e3e11e06cb343c2b118c17e6c6b0ef1ed756
Will <willazp@gmail.com>
2014-04-08 18:09:15 -0300

    Adicionadas as estruturas para as sub-listas de textos e ocorrências

--- src/Hash.h
+++ src/Hash.h
@@ -11,11 +11,32 @@
 class Hashish
 {
 private:
+
+    /* Na busca por ocorrências, primeiro deve-se encontrar a célula no hash, então a palavra na lista de colisões.
+    O No palavra possuíra uma sub-lista de textos com ocorrências, e cada NoTexto possuirá uma sub-lista de ocorrências.
+    Célula->Palavra->Texto->Lista de ocorrências */
+
+    struct NoOcorrencia
+    {
+        NoOcorrencia* prox;
+        int linha; //Posição da palavra nesta ocorrência. Pode não ser necessariamente a linha.
+    };
+    struct NoTexto
+    {
+        NoTexto* prox;
+        const char* arquivo;
+        int relev;    /*Relevância, deve ser incrementada a cada adição de ocorrência.
+                      Será usada como chave, pode ser usada para ordenar a lista de 'NoTexto's*/
+        NoOcorrencia* listaocor //Ponteiro para o começo da sub-lista contendo as ocorrências da palavra neste texto.
+    };
+
     struct No
     {
         No* prox;
         const char* palavra;
         No();
+
+        NoTexto* listatexto; //Ponteiro para o começo da sub-lista de textos nos quais a palavra aparece
     };
     struct Celula
     {
